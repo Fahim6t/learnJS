d74cb6510f35677f19abe423f12c5211e05da04b
Fahim <fahimbasha00@gmail.com>
2025-07-10 15:10:01 +0530

    updated

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
-// let username;
-// username = window.prompt("What is your username?");
-// console.log(username);
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+const countLabel = document.getElementById("countLabel");
+let count = 0;
 
-
-// let username;
-// document.getElementById("mySubmit").onclick = function(){
-//     username = document.getElementById("myText").value;
-//     document.getElementById("myH1").textContent=username;
-// }
-
-
-const PI = 3.14;
-let radius;
-let circumference;
-
-document.getElementById("mySubmit").onclick = function(){
-    radius = document.getElementById("myRadius").value;
-    radius = Number(radius);
-    circumference = 2 * PI * radius;
-    document.getElementById("myH3").textContent = circumference +"cm";
+increaseBtn.onclick = function(){
+    count++;
+    countLabel.textContent = count;
 }
 
+decreaseBtn.onclick = function(){
+    count--;
+    countLabel.textContent = count;
+}
 
+resetBtn.onclick = function(){
+    count = 0;
+    countLabel.textContent = count;
+}
